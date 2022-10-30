326ff98b71da2b7938b89723ad29baf0b8eaf6f2
Theo Liberman <liberman.theo@gmail.com>
2022-10-30 14:20:50 +0100

    void data

--- src/forms/SettingsDialog.cpp
+++ src/forms/SettingsDialog.cpp
@@ -28,9 +28,9 @@ void SettingsDialog::showEvent(QShowEvent *)
 	blog(LOG_INFO, "Show event");
 }
 
-bool SettingsDialog::EnumAudioSources(void *stx, obs_source_t *source)
+bool SettingsDialog::EnumAudioSources(void *data, obs_source_t *source)
 {
-	UNREFERENCED_PARAMETER(stx);
+	(void)data; 
 
 	uint32_t flags = obs_source_get_output_flags(source);
 
