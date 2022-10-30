f5cfcf05fd2dd128f6ff3759b11b47d3456e6041
Theo Liberman <liberman.theo@gmail.com>
2022-10-30 19:57:19 +0100

    Rewrite enums in showEvent

--- src/forms/SettingsDialog.cpp
+++ src/forms/SettingsDialog.cpp
@@ -8,18 +8,6 @@ SettingsDialog::SettingsDialog(QWidget *parent)
 	: QDialog(parent, Qt::Dialog), ui(new Ui::SettingsDialog)
 {
 	ui->setupUi(this);
-	auto cb = [this](obs_source_t *source) {
-		const char *name = obs_source_get_name(source);
-		blog(LOG_INFO, "Source : %s", name);
-		return true;
-	};
-
-	using cb_t = decltype(cb);
-	obs_enum_sources(
-		[](void *d, obs_source_t *source) {
-			return (*static_cast<cb_t *>(d))(source);
-		},
-		&cb);
 }
 
 SettingsDialog::~SettingsDialog()
@@ -33,4 +21,20 @@ void SettingsDialog::ToggleShowHide()
 		setVisible(true);
 	else
 		setVisible(false);
+}
+
+void SettingsDialog::showEvent(QShowEvent *)
+{
+	auto cb = [this](obs_source_t *source) {
+		char *name = obs_source_get_name(source);
+		blog(LOG_INFO, "Source : %s", name) return true;
+	};
+
+	using cb_t = decltype(cb);
+
+	obs_enum_sources(
+		[](void *d, obs_source_t *source) {
+			return (*static_cast<cb_t *>(d))(source);
+		},
+		&cb);
 }
