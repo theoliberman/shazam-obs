ec422e2bcd5bf1e009b1e4684dacce4cf1cb829e
Theo Liberman <liberman.theo@gmail.com>
2022-10-30 02:21:52 +0100

    Try to add audio sources to ComboBox

--- src/forms/SettingsDialog.cpp
+++ src/forms/SettingsDialog.cpp
@@ -3,14 +3,22 @@
 
 #include "SettingsDialog.h"
 
+bool enum_audio_sources(void *ui, obs_source_t *source)
+{
+	uint32_t flags = obs_source_get_output_flags(source);
+
+	if ((flags & OBS_SOURCE_AUDIO) != 0) {
+		const char *name = obs_source_get_name(source);
+		ui->comboBox->addItem(name)
+	}
+	return true;
+}
+
 SettingsDialog::SettingsDialog(QWidget *parent)
 	: QDialog(parent, Qt::Dialog), ui(new Ui::SettingsDialog)
 {
 	ui->setupUi(this);
-	ui->comboBox->addItem("A");
-	ui->comboBox->addItem("B");
-	ui->comboBox->addItem("C");
-	ui->comboBox->addItem("D");
+	obs_enum_sources(enum_audio_sources, ui);
 }
 
 SettingsDialog::~SettingsDialog()
