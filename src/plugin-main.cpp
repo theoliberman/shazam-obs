b1cd05594b9972f545df699e0ae69fc913c18103
Theo Liberman <liberman.theo@gmail.com>
2022-10-31 13:52:05 +0100

    Tweak ptr

--- src/plugin-main.cpp
+++ src/plugin-main.cpp
@@ -28,13 +28,13 @@ OBS_DECLARE_MODULE()
 OBS_MODULE_USE_DEFAULT_LOCALE(PLUGIN_NAME, "en-US")
 
 SettingsDialog *_settingsDialog = nullptr;
-config_t *_config = nullptr;
+config_t **_config = nullptr;
 
 bool obs_module_load(void)
 {
 
 	int confInt =
-		config_open_string(*_config, "ShazamOBS", CONFIG_OPEN_ALWAYS)
+		config_open_string(_config, "ShazamOBS", CONFIG_OPEN_ALWAYS)
 
 			obs_frontend_push_ui_translation(obs_module_get_string);
 	QMainWindow *mainWindow =
